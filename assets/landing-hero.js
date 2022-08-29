259f964b382a0e74b22ee70383599e21f7fb474f
Davis Judris <davis.judris@ecenta.com>
2022-08-29 21:55:20 +0300

    Country modal implemented, added code for region change

--- assets/landing-hero.js
+++ assets/landing-hero.js
@@ -1,3 +1,40 @@
+class LocalizationForm extends HTMLElement {
+    constructor() {
+        super();
+        this.elements = {
+            input: this.querySelector('input[name="language_code"], input[name="country_code"]'),
+        };
+        this.addEventListener('keyup', this.onContainerKeyUp.bind(this));
+
+        this.querySelectorAll('a').forEach(item => item.addEventListener('click', this.onItemClick.bind(this)));
+    }
+
+
+    onContainerKeyUp(event) {
+        if (event.code.toUpperCase() !== 'ESCAPE') return;
+        this.elements.button.focus();
+    }
+
+    onItemClick(event) {
+        event.preventDefault();
+        const form = this.querySelector('form');
+        this.elements.input.value = event.currentTarget.dataset.value;
+        if (form) form.submit();
+    }
+
+    openSelector() {
+        this.elements.button.focus();
+        this.elements.button.setAttribute('aria-expanded', (this.elements.button.getAttribute('aria-expanded') === 'false').toString());
+    }
+
+    closeSelector(event) {
+        const shouldClose = event.relatedTarget && event.relatedTarget.nodeName === 'BUTTON';
+    }
+}
+
+customElements.define('localization-form', LocalizationForm);
+
+
 $(document).ready(function () {
     if ($(window).width() < 992) {
         startCarousel();
