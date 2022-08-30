7058edd361073f8b1bd7df2effc48c5e97009e77
Davis Judris <davis.judris@ecenta.com>
2022-08-30 19:10:25 +0300

    Modal fix to work on different pages

--- assets/landing-hero.js
+++ assets/landing-hero.js
@@ -1,38 +1,5 @@
-class LocalizationForm extends HTMLElement {
-    constructor() {
-        super();
-        this.elements = {
-            input: this.querySelector('input[name="language_code"], input[name="country_code"]'),
-        };
-        this.querySelectorAll('a').forEach(item => item.addEventListener('click', this.onItemClick.bind(this)));
-    }
-
-    onItemClick(event) {
-        event.preventDefault();
-        const form = this.querySelector('form');
-        this.elements.input.value = event.currentTarget.dataset.value;
-        $('.js--country-modal').addClass('d-none')
-        $('body').removeClass('overflow-hidden')
-        if (form) form.submit();
-    }
-}
-
-customElements.define('localization-form', LocalizationForm);
-
 
 $(document).ready(function () {
-    let localizationCookie = document.cookie.match(new RegExp('(^| )' + 'localization' + '=([^;]+)'));
-    let localizationModal = $('.js--country-modal');
-    if (localizationCookie) {
-        localizationModal.addClass('d-none')
-        $('body').removeClass('overflow-hidden')
-    } else {
-        $('body').addClass('overflow-hidden')
-    }
-    $('.js--country-modal-close').click(function () {
-        localizationModal.addClass('d-none')
-        $('body').removeClass('overflow-hidden')
-    })
     if ($(window).width() < 992) {
         startCarousel();
     } else {
