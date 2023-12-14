c64955474eb851e8cb1b64c6dcb961f9f794d228
Edgars Pastors <edgars.pastors@kalmars.lv>
2023-12-14 15:13:17 +0200

    #reviews, best sellers, hero adjustments

--- assets/best-seller.js
+++ assets/best-seller.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     let owl = $("#best_seller_carousel").owlCarousel({
         navigation: false, // Show next and prev buttons
-        dots: true,
+        dots: false,
         autoplay: false,
         loop: false,
         nav: false,
@@ -10,56 +10,54 @@ $(document).ready(function () {
             0: {
                 items: 1,
                 dotsEach: 1,
-                dots: true,
                 margin: 20,
-
+                stagePadding: 30,
             },
             768: {
                 items: 2,
-                dots: true,
                 margin: 30,
 
             },
             992: {
                 items: 3,
-                margin: 30,
+                margin: 24,
             }
         },
 
     });
-    $('.customNextBtn').click(function () {
-        owl.trigger('next.owl.carousel');
-    })
-    $('.customPrevBtn').click(function () {
-        owl.trigger('prev.owl.carousel');
-    })
-
-    owl.on('changed.owl.carousel', function (event) {
-        let index = event.item.index;
-        let count = event.item.count
-
-
-        $('.customPrevBtn').removeClass("disabled");
-        $('.customNextBtn').removeClass("disabled");
-        if (index === 0) {
-            $('.customPrevBtn').addClass("disabled");
-        }
-
-        if (screen.width >= 992) {
-            if (index === count - 3) {
-                $('.customNextBtn').addClass("disabled");
-            }
-        }
-        if (screen.width >= 768 && screen.width < 992) {
-            if (index === count - 2) {
-                $('.customNextBtn').addClass("disabled");
-            }
-        }
-        if (screen.width < 768) {
-            if (index === count - 1) {
-                $('.customNextBtn').addClass("disabled");
-            }
-        }
-    });
+    // $('.customNextBtn').click(function () {
+    //     owl.trigger('next.owl.carousel');
+    // })
+    // $('.customPrevBtn').click(function () {
+    //     owl.trigger('prev.owl.carousel');
+    // })
+    //
+    // owl.on('changed.owl.carousel', function (event) {
+    //     let index = event.item.index;
+    //     let count = event.item.count
+    //
+    //
+    //     $('.customPrevBtn').removeClass("disabled");
+    //     $('.customNextBtn').removeClass("disabled");
+    //     if (index === 0) {
+    //         $('.customPrevBtn').addClass("disabled");
+    //     }
+    //
+    //     if (screen.width >= 992) {
+    //         if (index === count - 3) {
+    //             $('.customNextBtn').addClass("disabled");
+    //         }
+    //     }
+    //     if (screen.width >= 768 && screen.width < 992) {
+    //         if (index === count - 2) {
+    //             $('.customNextBtn').addClass("disabled");
+    //         }
+    //     }
+    //     if (screen.width < 768) {
+    //         if (index === count - 1) {
+    //             $('.customNextBtn').addClass("disabled");
+    //         }
+    //     }
+    // });
 });
 
