6596bd1e47121d8c8858da0db9bb44f5a768be42
ProtoGod <ruslans.sedlarovs@kalmars.lv>
2024-02-23 14:29:30 +0200

    - fixed landing style - started work with collection page

---
+++ assets/learn-more.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let summary = document.getElementById('summary');
+    let collapseText = summary.querySelector('.collapse');
+    let collapseButton = summary.querySelector('span');
+
+    function isTextClamped(element) {
+        return element.scrollHeight > element.clientHeight;
+    }
+
+    if (!isTextClamped(collapseText)) {
+        collapseButton.style.display = 'none';
+    } else {
+        collapseButton.style.display = 'inline';
+    }
+
+    collapseButton.addEventListener('click', function (event) {
+        let isCollapsed = collapseText.classList.contains('show');
+        if (isCollapsed) {
+            collapseText.classList.remove('show');
+            collapseText.style.display = '-webkit-box';
+            collapseButton.textContent = 'Learn More';
+            collapseButton.classList.add('collapsed');
+        } else {
+            // If the text is collapsed, expand it
+            collapseText.classList.add('show');
+            collapseText.style.display = 'block';
+            collapseButton.textContent = '';
+            collapseButton.classList.remove('collapsed');
+        }
+    });
+});
